c891b70e1240c3d96a86af4f3235125801e671f0
pixel <pixel+github@chrissx.de>
2023-09-23 21:21:58 +0200

    0.1.0
    - support for half-steps in between the full ratings
    - some better ordering etc for the kinks (also some prep...)

--- src/components/Rater.tsx
+++ src/components/Rater.tsx
@@ -1,11 +1,12 @@
-import { ratingColors } from "../base";
+import { extraRatingColor, ratingColors } from "../base";
 import { useState } from "preact/hooks";
 
 export default function Rater({ text }: { text: string }) {
     const [rating, setRating] = useState(0);
+    // TODO: use right click to go back
     return (
-        <div class="rater" onClick={() => setRating((rating + 1) % ratingColors.length)}>
-            <button style={"background-color:" + ratingColors[rating]} />
+        <div class="rater" onClick={(e) => setRating((rating + (e.shiftKey ? 0.5 : 1)) % ratingColors.length)}>
+            <button style={"background-color:" + extraRatingColor(rating)} />
             {text}
         </div>
     );
