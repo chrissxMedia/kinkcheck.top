fab46abb1c762ab545f6c40f920fbce369adcfa5
pixel <pixel+github@chrissx.de>
2023-09-24 06:36:01 +0200

    Rater: add `initialRating` parameter

--- src/components/Rater.tsx
+++ src/components/Rater.tsx
@@ -1,8 +1,8 @@
 import { extraRatingColor, ratingColors } from "../base";
 import { useState } from "preact/hooks";
 
-export default function Rater({ text }: { text: string }) {
-    const [rating, setRating] = useState(0);
+export default function Rater({ text, initialRating }: { text: string, initialRating?: number }) {
+    const [rating, setRating] = useState(initialRating ?? 0);
     // TODO: use right click to go back
     return (
         <div class="rater" onClick={(e) => setRating((rating + (e.shiftKey ? 0.5 : 1)) % ratingColors.length)}>
