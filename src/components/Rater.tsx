8bafd0c209700df0e7e82ae466d404d5d94ff997
pixel <pixel+github@chrissx.de>
2023-09-24 07:05:17 +0200

    0.1.1: Rater: add right click support, and a small fix
    shift-clicking on the hard limit didn't do anything previously

--- src/components/Rater.tsx
+++ src/components/Rater.tsx
@@ -3,9 +3,15 @@ import { useState } from "preact/hooks";
 
 export default function Rater({ text, initialRating }: { text: string, initialRating?: number }) {
     const [rating, setRating] = useState(initialRating ?? 0);
-    // TODO: use right click to go back
+    const handleClick = (e: MouseEvent) => {
+        e.preventDefault();
+        const step = e.shiftKey ? 0.5 : 1;
+        const newRating = e.button ? rating - step : rating + step;
+        const max = ratingColors.length - 1;
+        setRating(newRating > max ? 0 : newRating < 0 ? max : newRating);
+    };
     return (
-        <div class="rater" onClick={(e) => setRating((rating + (e.shiftKey ? 0.5 : 1)) % ratingColors.length)}>
+        <div class="rater" onClick={handleClick} onContextMenu={handleClick}>
             <button style={"background-color:" + extraRatingColor(rating)} />
             {text}
         </div>
