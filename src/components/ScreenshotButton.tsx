5cb02667694c533b1c4772d3f4fe12696cb65744
pixel <pixel+github@chrissx.de>
2023-11-04 05:00:51 +0100

    0.4.1: repair the screenshots (for everyone!)

--- src/components/ScreenshotButton.tsx
+++ src/components/ScreenshotButton.tsx
@@ -4,20 +4,26 @@ export default function ScreenshotButton({ title }: { title: string }) {
     let download: HTMLAnchorElement;
     return (
         <div>
-            <a onClick={() =>
-                html2canvas(document.getElementById("content")!, {
+            <a onClick={async () => {
+                const content = document.getElementById("content")!;
+                content.style.width = "1440px";
+                const canvas = await html2canvas(content, {
                     backgroundColor: "black",
                     windowWidth: 1440,
-                    scale: 2,
-                }).then((canvas) => {
-                    const date = new Date().toISOString()
-                        .replace(/\....Z$/, "").replace("T", " ");
-                    download.setAttribute("download", `${title} ${date}.png`);
-                    download.setAttribute("href", canvas
-                        .toDataURL("image/png")
-                        .replace("image/png", "image/octet-stream"));
-                    download.click();
-                })}>Take a Screenshot</a>
+                    scrollX: 0,
+                    scrollY: 0,
+                    scale: window.orientation !== undefined ? 1 : 2,
+                });
+                content.style.width = "";
+                const date = new Date().toISOString()
+                    .replace(/\....Z$/, "").replace("T", " ");
+                download.setAttribute("download", `${title} ${date}.png`);
+                download.setAttribute("href", canvas
+                    .toDataURL("image/png")
+                    .replace("image/png", "image/octet-stream"));
+                // TODO: consider if it's empty to alert instead
+                download.click();
+            }}>Take a Screenshot</a>
             <a ref={x => download = x!} style="width:0;height:0" />
         </div>
     );
