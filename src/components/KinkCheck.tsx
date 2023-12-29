7e9d4ffb770953239de1a883ce1016dfbb525eac
pixel <pixel+github@chrissx.de>
2023-12-29 07:42:38 +0100

    add a broken first draft of a tutorial
    i have like no idea why it's broken but yea 🤷🏻‍♀️

--- src/components/KinkCheck.tsx
+++ src/components/KinkCheck.tsx
@@ -1,8 +1,25 @@
 import { useState } from "preact/hooks";
-import { defaultRatings, kinks, type kink } from "../base";
+import { defaultRatings, type kink, type metadata } from "../base";
 import Kink from "./Kink";
 import styles from "./KinkCheck.module.css";
 
+export function ExampleTable({ kinks }: { kinks: kink[] }) {
+    const [ratings, setRatings] = useState(kinks.map(([, positions]) => positions.map(() => 0)));
+    const setRating = (kink: number) => (pos: number) => (rat: number) => {
+        const p = ratings[kink];
+        p[pos] = rat;
+        const r = ratings;
+        r[kink] = p;
+        console.log(r);
+        setRatings(r);
+    };
+    console.log(ratings);
+    return <table class={styles.table}>
+        <tbody>
+            {kinks.map((kink, i) => <Kink kink={kink} ratings={ratings[i]} setRating={setRating(i)} />)}
+        </tbody>
+    </table>;
+}
 
 export function Category({ cat, kinks, ratings, setRating }: {
     cat: string, kinks: kink[], ratings: number[][],
@@ -22,8 +39,8 @@ export function Category({ cat, kinks, ratings, setRating }: {
     );
 }
 
-export default function KinkCheck() {
-    const [ratings, setRatings] = useState(defaultRatings);
+export default function KinkCheck(meta: metadata) {
+    const [ratings, setRatings] = useState(defaultRatings(meta.kinks));
     const setRating = (cat: string) => (kink: number) => (pos: number) => (rat: number) => {
         const c = ratings[cat];
         const p = c[kink];
@@ -34,7 +51,7 @@ export default function KinkCheck() {
     // TODO: add a name field
     return <main class={styles.catcontainer}>
         {
-            Object.entries(kinks).map(([cat, kinks]) => (
+            Object.entries(meta.kinks).map(([cat, kinks]) => (
                 <Category cat={cat} kinks={kinks} ratings={ratings[cat]} setRating={setRating(cat)} />
             ))
         }
